5fea09ae85c2cba4a0673f822897da738806dea9
VipSwap <vipswappro@gmail.com>
2021-05-24 11:08:21 +0800

    edit

--- packages/uikit/src/widgets/WalletModal/types.ts
+++ packages/uikit/src/widgets/WalletModal/types.ts
@@ -5,6 +5,7 @@ export enum ConnectorNames {
   Injected = "injected",
   WalletConnect = "walletconnect",
   BSC = "bsc",
+  OKT = "okt",
 }
 
 export type Login = (connectorId: ConnectorNames) => void;
